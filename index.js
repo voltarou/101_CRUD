c9618073b9e6b6ed84a43cd989ee504f92742a91
voltarou <161274983+voltarou@users.noreply.github.com>
2025-10-21 14:15:10 +0700

    const express

---
+++ index.js
@@ -0,0 +1,6 @@
+const express = require('express');
+let mysql = require('mysql2');
+const app = express();
+const port = 3000;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
